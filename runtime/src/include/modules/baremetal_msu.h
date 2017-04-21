a7a0b0430e36afcadf7bfc389bdd738e12889461
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-04-21 13:42:25 -0400

    listen sock for baremetal data flow

--- runtime/src/include/modules/baremetal_msu.h
+++ runtime/src/include/modules/baremetal_msu.h
@@ -1,13 +1,16 @@
 #ifndef BAREMETAL_MSU_H
 #define BAREMETAL_MSU_H
 #include "generic_msu.h"
+#include "ssl_msu.h" //for definition of READ and WRITE type
 
 /** Holds data to be delivered to a baremetal MSU */
 struct baremetal_msu_data_payload {
+    int type;
+    int socketfd;
     uint64_t int_data;
 };
 
 
 const struct msu_type BAREMETAL_MSU_TYPE;
-
+struct generic_msu *baremetal_entry_msu;
 #endif
