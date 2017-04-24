3e703f83c7cccaf4b4aca15d59eb7bc1b400444f
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-04-24 17:24:41 -0400

    first hop baremetal read, need to poll for established connections

--- runtime/src/include/modules/baremetal_msu.h
+++ runtime/src/include/modules/baremetal_msu.h
@@ -1,15 +1,18 @@
 #ifndef BAREMETAL_MSU_H
 #define BAREMETAL_MSU_H
 #include "generic_msu.h"
-#include "ssl_msu.h" //for definition of READ and WRITE type
 
 /** Holds data to be delivered to a baremetal MSU */
 struct baremetal_msu_data_payload {
     int type;
     int socketfd;
-    uint64_t int_data;
+    unsigned int int_data;
 };
+#define BAREMETAL_RECV_BUFFER_SIZE 64
 
+#define READ_FROM_SOCK 1
+#define FORWARD 2
+#define WRITE_TO_SOCK 3
 
 const struct msu_type BAREMETAL_MSU_TYPE;
 struct generic_msu *baremetal_entry_msu;
