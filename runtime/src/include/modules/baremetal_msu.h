77dae4399aa89b00aff9cbf84841cf93c6ee916b
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-04-24 23:03:28 -0400

    init and free internal state for entry baremetal msu

--- runtime/src/include/modules/baremetal_msu.h
+++ runtime/src/include/modules/baremetal_msu.h
@@ -1,6 +1,16 @@
 #ifndef BAREMETAL_MSU_H
 #define BAREMETAL_MSU_H
+#include <sys/poll.h>
 #include "generic_msu.h"
+
+#define INITIAL_ACTIVE_SOCKETS_SIZE 10
+
+/** Internal state struct for tracking active connection and use in poll */
+struct baremetal_msu_internal_state {
+    int active_sockets;
+    int total_array_size;
+    struct pollfds *fds;
+};
 
 /** Holds data to be delivered to a baremetal MSU */
 struct baremetal_msu_data_payload {
@@ -19,4 +29,5 @@ const struct msu_type BAREMETAL_MSU_TYPE;
 struct generic_msu *baremetal_entry_msu;
 int baremetal_msu_init_entry(struct generic_msu *self,
         struct create_msu_thread_msg_data *create_action);
+void baremetal_msu_destroy_entry(struct generic_msu *self);
 #endif
