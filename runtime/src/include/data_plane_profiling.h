aaaddc593790299f598db079deee4a971483f437
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-05-01 15:16:46 -0400

    data plane profiling header file and update generic msu queue struct

--- runtime/src/include/data_plane_profiling.h
+++ runtime/src/include/data_plane_profiling.h
@@ -1,12 +1,56 @@
 #ifndef DATA_PLANE_PROFILING_H_
 #define DATA_PLANE_PROFILING_H_
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include "logging.h"
+/** Number of entries per item that can be logged from entry to exit */
+#define MAX_DATAPLANE_ITEMS 64
+#define MAX_DATAPLANE_LOG_ENTRY_LEN 512
+#define CLOCK_ID CLOCK_MONOTONIC
 
-#ifdef DATAPLANE_PROFILING
-/* declare everything here */
+typedef enum {
+    ENQUEUE = 0,
+    DEQUEUE,
+    REMOTE_SEND, //When sent over TCP conn b/w runtimes
+    REMOTE_RECV //When recevied over TCP conn b/w runtimes
+} enum_dataplane_op_id;
 
+static const char *enum_dataplane_op_name[] = {
+    "ENQUEUE",
+    "DEQUEUE",
+    "REMOTE_SEND", //When sent over TCP conn b/w runtimes
+    "REMOTE_RECV" //When recevied over TCP conn b/w runtimes
+};
 
+struct dataplane_profile_info {
+    /** Unique id to be assigned to each packet at entry point of dedos */
+    int dp_id;
+    /** Current count of dataplane log entries (dp_entry_count < MAX_DATAPLANE_ITEMS) */
+    int dp_entry_count;
+    /** 2d char array to hold MAX_DATAPLANE_ITEMS logs items with len MAX_DATAPLANE_LOG_ENTRY_LEN */
+    char dp_log_entries[MAX_DATAPLANE_ITEMS][MAX_DATAPLANE_LOG_ENTRY_LEN];
+};
 
-
-#endif /* DATAPLANE_PROFILING */
+static inline log_dp_event(int msu_id, enum_dataplane_op_id dataplane_op_id,
+    struct dataplane_profile_info *dp_profile_info)
+{
+    struct timespec cur_timestamp;
+    long int ts;
+    clock_gettime(CLOCK_ID, &cur_timestamp);
+    ts = (cur_timestamp.tv_sec * 1000 * 1000) + (cur_timestamp.tv_nsec / 1000);
+    snprintf(dp_profile_info->dp_log_entries[dp_profile_info->dp_entry_count],
+            MAX_DATAPLANE_LOG_ENTRY_LEN,
+            "%ld %d, %d, %d, %s",
+            ts, dp_profile_info->dp_id, dp_profile_info->dp_entry_count,
+                msu_id, enum_dataplane_op_name[dataplane_op_id]);
+    log_warn("LOGGED: %s",dp_profile_info->dp_log_entries[dp_profile_info->dp_entry_count]);
+    dp_profile_info->dp_entry_count++;
+}
+// #ifdef DATAPLANE_PROFILING
+// /* declare everything here */
+// #endif /* DATAPLANE_PROFILING */
 
 #endif /* DATA_PLANE_PROFILING_H_ */
