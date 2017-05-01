dcdeb3c774b3767b3fc435fa35e61ade3d014672
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-05-01 10:26:55 -0400

    Makefile update to enable DATAPLANE_PROFILING

---
+++ runtime/src/include/data_plane_profiling.h
@@ -0,0 +1,12 @@
+#ifndef DATA_PLANE_PROFILING_H_
+#define DATA_PLANE_PROFILING_H_
+
+#ifdef DATAPLANE_PROFILING
+/* declare everything here */
+
+
+
+
+#endif /* DATAPLANE_PROFILING */
+
+#endif /* DATA_PLANE_PROFILING_H_ */
