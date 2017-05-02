af91eea36f47cce123f8091efacc60d3d0c93e5b
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-05-02 16:31:33 -0400

    data profile log level

--- runtime/src/include/logging.h
+++ runtime/src/include/logging.h
@@ -18,7 +18,7 @@
 #define WARN 1
 #define TCP_DEBUG 0
 #define STATS 0
-
+#define DATA_PROFILING 1
 
 #define log_error(fmt, ...) \
         do { if (ERROR) { fprintf(stderr, "" ANSI_COLOR_RED "%lu:%s:%d:%s(): ERROR: " fmt, pthread_self(),__FILE__, \
@@ -59,6 +59,16 @@
 #else
 #define log_stats(...)
 #endif
+
+#ifdef DATA_PROFILING
+#define log_profile(fmt, ...) \
+        do { if (1) { fprintf(stderr,  "%lu:%s:%d:%s(): DATA_PROFILE: " fmt "\n", pthread_self(),__FILE__, \
+                                __LINE__, __func__, ##__VA_ARGS__); \
+        }} while (0)
+#else
+#define log_profile(...)
+#endif
+
 
 #define tcp_dbg(fmt, ...) \
         do { if (TCP_DEBUG) { fprintf(stderr, "%lu:%s:%d:%s(): TCP_DEBUG: " fmt, pthread_self(),__FILE__, \
