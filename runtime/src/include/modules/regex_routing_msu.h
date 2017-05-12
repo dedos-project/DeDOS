bfd6a6acbba5cd4823856efe16d1da784f54061e
Isaac Pedisich <iped@seas.upenn.edu>
2017-05-12 15:22:04 -0400

    adding regex_routing_msu for queuelength based routing

---
+++ runtime/src/include/modules/regex_routing_msu.h
@@ -0,0 +1,7 @@
+#ifndef REGEX_ROUTING_MSU_H
+#define REGEX_ROUTING_MSU_H
+#include "comm_protocol.h"
+
+const struct msu_type REGEX_ROUTING_MSU_TYPE;
+
+#endif
---
+++ runtime/src/modules/regex_routing_msu.c
@@ -0,0 +1,24 @@
+#include "modules/regex_routing_msu.h"
+#include "modules/regex_msu.h"
+#include "dedos_msu_list.h"
+#include "dedos_msu_msg_type.h"
+#include "modules/webserver_msu.h"
+
+static int regex_routing_receive(struct generic_msu *self, msu_queue_item *queue_item){
+    return DEDOS_REGEX_MSU_ID;
+}
+
+const struct msu_type REGEX_ROUTING_MSU_TYPE = {
+    .name="regex_routing_msu",
+    .layer=DEDOS_LAYER_APPLICATION,
+    .type_id=DEDOS_REGEX_ROUTING_MSU_ID,
+    .proto_number=MSU_PROTO_REGEX,
+    .init=NULL,
+    .destroy=NULL,
+    .receive=regex_routing_receive,
+    .receive_ctrl=NULL,
+    .route=round_robin,
+    .deserialize=regex_deserialize,
+    .send_local=default_send_local,
+    .send_remote=default_send_remote
+};
