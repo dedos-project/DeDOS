8a26abf369db983098203dfea4865fbe357ab44a
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-05-19 15:41:54 -0400

    updated header files from old branch

--- runtime/src/include/modules/msu_pico_tcp.h
+++ runtime/src/include/modules/msu_pico_tcp.h
@@ -2,6 +2,8 @@
 #define MSU_PICO_TCP_H
 
 #include "generic_msu.h"
+#include "generic_msu_queue.h"
+#include "generic_msu_queue_item.h"
 
 struct msu_type PICO_TCP_MSU_TYPE;
 struct generic_msu *pico_tcp_msu;
