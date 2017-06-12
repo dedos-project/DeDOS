35f78e645629e8f49549665815efab3e8aaac033
Tavish Vaidya <tavish@cs.georgetown.edu>
2017-06-12 18:55:32 -0400

    calls to generate id for tcp flows, need to fill generate_id functionality

--- runtime/src/include/modules/msu_pico_tcp.h
+++ runtime/src/include/modules/msu_pico_tcp.h
@@ -4,8 +4,10 @@
 #include "generic_msu.h"
 #include "generic_msu_queue.h"
 #include "generic_msu_queue_item.h"
+#include "pico_frame.h"
 
 struct msu_type PICO_TCP_MSU_TYPE;
 struct generic_msu *pico_tcp_msu;
+int pico_tcp_generate_id(struct generic_msu *self, struct pico_frame *f);
 
 #endif
