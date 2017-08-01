540fdd374551a5d81d3242e3b3b46ea2bded9e31
maxdml <maxdml@seas.upenn.edu>
2017-08-01 09:37:01 -0400

    dataplane profiler now uses CLOCK_MONOTONIC, and requests ids have a configurable offset (quick hack to distinguish requests between multiple runtimes)

--- runtime/src/stack/data_plane_profiling.c
+++ runtime/src/stack/data_plane_profiling.c
@@ -2,29 +2,33 @@
 #include <assert.h>
 #include <stdio.h>
 
+#define REQUEST_ID_OFFSET 1000000
+
 //#ifdef DATAPLANE_PROFILING
-unsigned long int get_request_id(){
+unsigned long int get_request_id() {
     static unsigned long int counter;
     unsigned long int ret_val = 0;
-    if(counter == 0){
-        srand(time(NULL));   // should only be called once
+
+    if (counter == 0) {
+        srand(time(NULL)); // should only be called once
     }
+
     float r = rand() % 10;
-    if(r > dprof_tag_probability * 10.0){
+    if (r > dprof_tag_probability * 10.0) {
         //Do not tag the request, i.e. not track this
         return ret_val;
     }
+
     pthread_mutex_lock(&request_id_mutex);
     counter++;
-    ret_val = counter;
+    ret_val = counter + REQUEST_ID_OFFSET;
     pthread_mutex_unlock(&request_id_mutex);
 
     return ret_val;
 }
 
-int init_data_plane_profiling(void){
-    if (pthread_mutex_init(&request_id_mutex, NULL) != 0)
-    {
+int init_data_plane_profiling(void) {
+    if (pthread_mutex_init(&request_id_mutex, NULL) != 0) {
         log_error("request id mutex init failed");
         return 1;
     }
@@ -43,8 +47,7 @@ int init_data_plane_profiling(void){
 */
 
     /* Skipping in memory log and directly write to log since its buffered */
-    if (pthread_mutex_init(&fp_log_mutex, NULL) != 0)
-    {
+    if (pthread_mutex_init(&fp_log_mutex, NULL) != 0) {
         log_error("FP log file mutex init failed");
         return 1;
     }
@@ -63,7 +66,7 @@ int init_data_plane_profiling(void){
     return 0;
 }
 
-int deinit_data_plane_profiling(void){
+int deinit_data_plane_profiling(void) {
     pthread_mutex_lock(&fp_log_mutex);
     fclose(fp_log);
     pthread_mutex_unlock(&fp_log_mutex);
@@ -71,14 +74,14 @@ int deinit_data_plane_profiling(void){
     return 0;
 }
 
-void clear_in_memory_profile_log(void){
+void clear_in_memory_profile_log(void) {
     memset(&mem_dp_profile_log.in_memory_entries,'\0',
             sizeof(char)*MAX_DATAPLANE_IN_MEMORY_LOG_ITEMS * MAX_DATAPLANE_LOG_ENTRY_LEN);
     mem_dp_profile_log.in_memory_entry_count = 0;
     mem_dp_profile_log.in_memory_entry_max_capacity = MAX_DATAPLANE_IN_MEMORY_LOG_ITEMS;
 }
 
-int dump_profile_logs(char *logfile){
+int dump_profile_logs(char *logfile) {
     FILE *fp;
     char *backup_log_file = "dataplane_profile.log\0";
     if(!logfile){
@@ -100,7 +103,7 @@ int dump_profile_logs(char *logfile){
     }
     fclose(fp);
     clear_in_memory_profile_log();
-    
+
     pthread_mutex_unlock(&mem_dp_profile_log.mutex);
     return 0;
 }
