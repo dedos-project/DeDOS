13c30f95681befba1c33c913edddd2516d777da5
maxdml <maxdml@seas.upenn.edu>
2017-08-08 11:01:22 -0400

    update webserver msu type for global controller

--- global_controller/src/stat_msg_handler.c
+++ global_controller/src/stat_msg_handler.c
@@ -36,9 +36,9 @@ struct cloning_info {
 };
 
 struct cloning_info CLONING_INFO[] = {
-// { DEDOS_SSL_READ_MSU_ID },
-//    { DEDOS_WEBSERVER_MSU_ID },
-     { DEDOS_WEBSERVER_REGEX_MSU_ID },
+//    { DEDOS_SSL_READ_MSU_ID },
+    { DEDOS_WEBSERVER_READ_MSU_ID },
+//     { DEDOS_WEBSERVER_REGEX_MSU_ID },
 };
 
 int CLONING_INFO_LEN = sizeof(CLONING_INFO) / sizeof(struct cloning_info);
@@ -83,7 +83,6 @@ int gather_cloning_info(struct stats_control_payload *stats, struct cloning_info
 }
 
 int make_cloning_decision(struct cloning_info *cloning_info, int info_len, struct dfg_vertex **to_clone) {
-
     double sum_stat[info_len];
     for (int i = 0; i < info_len; i++) {
         sum_stat[i] = 0;
@@ -138,7 +137,7 @@ void set_haproxy_weights() {
     int n_runtimes = 0;
     for (int i = 0; i < dfg->vertex_cnt; i++) {
         struct dfg_vertex *msu = dfg->vertices[i];
-        if (msu->msu_type == DEDOS_SSL_READ_MSU_ID) {
+        if (msu->msu_type == DEDOS_WEBSERVER_READ_MSU_ID) {
             n_handshakes[msu->scheduling.runtime->id]++;
             if (n_runtimes < msu->scheduling.runtime->id)
                 n_runtimes = msu->scheduling.runtime->id;
@@ -225,59 +224,3 @@ int process_stats_msg(struct stats_control_payload *stats, int runtime_sock) {
 #endif
     return 0;
 }
-
-
-    /*
-    for (int i = 0; i < stats->n_samples; i++) {
-        struct stat_sample *sample = &stats->samples[i];
-
-        // Will have to move once we get non-msu stats
-        struct dfg_vertex *msu = get_msu_from_id(sample->item_id);
-
-        if (msu == NULL) {
-            log_error("Could not get MSU with ID %d", sample->item_id);
-            return -1;
-        }
-
-
-        int ret;
-        switch (sample->stat_id) {
-            case QUEUE_LEN:
-                //There is an odious situation bellow. Can you figure it out?
-                if (msu->msu_type == DEDOS_SSL_READ_MSU_ID) {
-                    if (average(get_msu_from_id(sample->item_id), sample->stat_id) > 0) {
-                        struct msus_of_type *http_dep = get_msus_from_type(DEDOS_WEBSERVER_MSU_ID);
-
-                        errored = clone_msu(http_dep->msu_ids[0]);
-                    }
-                }
-
-//                if (average(get_msu_from_id(sample->item_id), sample->stat_id) > 5) {
-//                    clone_msu(17);
-//                }
-*/
-/*
-                if (msu->msu_type == DEDOS_SSL_READ_MSU_ID) {
-                    struct msus_of_type *sslreads = get_msus_from_type(500);
-                    //Assume fixed initial number of MSUs on rt1
-                    int n_msu = sslreads->num_msus - 6;
-
-                    if (sample->stats->stat > 5) {
-                        ret = clone_msu(msu->msu_id);
-                        sleep(5);
-                        if (ret != -1) {
-                            const char cmd[64];
-                            snprintf(cmd, 64, "echo 'set weight https/s2 %d' | socat stdio /tmp/haproxy.socket", n_msu + 1);
-                            system(cmd);
-                        }
-                    }
-                }
-                break;
-
-            default:
-                //log_debug("No action implemented for stat_id %d", sample->stat_id);
-                continue;
-        }
-    }
-*/
-
