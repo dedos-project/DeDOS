7987d1bb19363774cd967b770f9247187ccc83e3
maxdml <maxdml@seas.upenn.com>
2017-11-27 17:50:35 -0500

    restore some code lost during rebase + add dbscan_predict for tryout

--- analytic/engine/classifiers.py
+++ analytic/engine/classifiers.py
@@ -1,9 +1,39 @@
 #!/usr/bin/python
 
+from sklearn import svm
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.ensemble import IsolationForest
+import scipy as sp
 
 def do_kmeans(X, k):
-    k_means = KMeans(n_clusters = k)
-    k_means.fit(X)
+    return KMeans(n_clusters = k).fit(X)
 
-    return k_means
+def do_dbscan(X, e, m):
+    return DBSCAN(eps = e, min_samples = m).fit(X)
+
+def do_lof(k):
+    return LocalOutlierFactor(n_neighbors = k)
+
+def do_svm(X, k, g):
+    return svm.OneClassSVM(kernel = k, gamma = g).fit(X)
+
+def do_isolation_forest(X, m, r):
+    return IsolationForest(max_samples = m, random_state = r).fit(X)
+
+#https://stackoverflow.com/questions/27822752/scikit-learn-predicting-new-points-with-dbscan
+def dbscan_predict(model, X_new, metric=sp.spatial.distance.cosine):
+    # Result is noise by default
+    y_new = np.ones(shape = len(X_new), dtype=int) * -1
+
+    # Iterate all input samples for a label
+    for j, x_new in enumerate(X_new):
+        # Find a core sample closer than EPS
+        for i, x_core in enumerate(model.components_):
+            if metric(x_new, x_core) < model.eps:
+                # Assign label of x_core to x_new
+                y_new[j] = model.labels_[model.core_sample_indices_[i]]
+                break
+
+    return y_new
--- analytic/engine/feature_engineering.py
+++ analytic/engine/feature_engineering.py
@@ -1,30 +1,22 @@
 import os
 import sys
 import pandas as pd
-from classifiers import *
+from sklearn import preprocessing
 
-sys.path.append(os.path.abspath(os.path.dirname(__file__)) + '/../database/')
+def normalize_df(df, fields):
+    min_max_scaler = preprocessing.MinMaxScaler()
 
-from db_api import *
+    for field in fields:
+        df[field] = min_max_scaler.fit_transform(df[field].values.reshape(-1,1))
 
-db = db_api()
-msus = db.get_items('msus')
+def gen_activity_ratio(df):
+    non_zero = df.loc[(df.EXEC_TIME > 0) & (df.IDLE_TIME > 0)]
+    act_ratio = non_zero.EXEC_TIME / (non_zero.EXEC_TIME + non_zero.IDLE_TIME)
+    df.loc[(df.EXEC_TIME > 0) & (df.IDLE_TIME > 0), 'ACTIVITY_RATIO'] = act_ratio
+    df.ACTIVITY_RATIO = df.ACTIVITY_RATIO.fillna(0)
 
-for msu in msus:
-    print msu.msu_type.name
-
-ts = db.get_msu_timeseries(msus[0])
-
-pts = db.get_ts_points(ts[0])
-
-vals = [pt.val for pt in pts]
-times = [pt.ts for pt in pts]
-
-df = pd.DataFrame({'timestamp': times,
-                  'value'     :vals})
-
-df = db.get_msu_full_df(msus[0])
-
-X = df.loc[:, ['QUEUE_LEN', 'EXEC_TIME', 'IDLE_TIME']]
-
-k_means_model = do_kmeans(X, 5)
+def gen_mem_per_state(df):
+    non_zero = df.loc[(df.NUM_STATES > 0) & (df.MEMORY_ALLOCATED > 0)]
+    mem_ratio = non_zero.MEMORY_ALLOCATED / non_zero.NUM_STATES
+    df.loc[(df.NUM_STATES > 0) & (df.MEMORY_ALLOCATED > 0), 'MEM_PER_STATE'] = mem_ratio
+    df.MEM_PER_STATE = df.MEM_PER_STATE.fillna(0)
