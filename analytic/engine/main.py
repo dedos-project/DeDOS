eaa4bdf6ec3fd96776afcb6e09238fbcda725336
maxdml <maxdml@seas.upenn.com>
2017-12-01 18:57:19 -0500

    add python script to load data from mysql, and fetch new data periodically

---
+++ analytic/engine/main.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import os
+import sys
+import time
+import matplotlib.pyplot as plt
+
+sys.path.append(os.path.abspath(os.getcwd() + '/../database'))
+import db_api
+
+from classifiers import *
+from feature_engineering import *
+
+def pre_process(df):
+    # Feature engineer
+    gen_activity_ratio(df)
+    gen_mem_per_state(df)
+
+    # Scale fields between {0,1}
+    scale_fields = ['EXEC_TIME', 'IDLE_TIME', 'QUEUE_LEN', 'MEMORY_ALLOCATED', 'MEM_PER_STATE']
+    normalize_df(df, scale_fields)
+
+# Load system info
+db = db_api.db_api()
+msus = db.get_items('msus')
+#FIXME: dirty length control to work with few msus first
+msus = [msus[3]]
+
+# Features to operate on
+fields = ['EXEC_TIME', 'IDLE_TIME', 'QUEUE_LEN', 'MEMORY_ALLOCATED', 'ACTIVITY_RATIO',
+          'MEM_PER_STATE']
+
+# Get all data from beginning of time and treat data
+dataframes = {}
+models = {}
+for msu in msus:
+    df = db.get_msu_full_df(msu)
+
+    pre_process(df)
+
+    #FIXME: simulate that we have only points up to 1.5119001064880338e+18
+    dataframes[msu] = df[df.TIME <= 1.5119001064880338e+18]
+
+    # Fit one model for each MSU
+    train_set = df.loc[:, fields]
+    models[msu] = do_dbscan(train_set, 0.3, 500)
+
+last_timestamp = dataframes[msus[0]].TIME.tail(1)
+
+while (True):
+    #TODO: check for new MSUs
+
+    # Get new data
+    for msu in msus:
+        print 'getting new data until:' + str(last_timestamp)
+        df = db.get_msu_full_df(msu, last_timestamp)
+        print df
+
+        if df.empty:
+            print 'nothing new, sleeping...'
+            time.sleep(1)
+            next
+        else:
+            pre_process(df)
+
+            print 'processing new data...'
+            X_new = df.loc[:, fields]
+            '''
+            for row in X_new.iterrows():
+                y_hat = dbscan_predict(models[msu], row)
+                if y_hat == -1:
+                    print "we have an outlier"
+            '''
+
+            last_timestamp = df.TIME.tail(1)
+            print 'updated timestamp:' + str(last_timestamp)
